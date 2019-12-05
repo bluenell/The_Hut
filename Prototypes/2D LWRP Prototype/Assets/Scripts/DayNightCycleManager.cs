a7ea41ac90e8d2bdc2558209a0aba9ec34ccb5ba
Jamie <jmb1g17@soton.ac.uk>
2019-12-05 15:29:32 +0000

    Lerp Transitions
    When you press SPACE, it smoothly transitions to the next colour

--- Prototypes/2D LWRP Prototype/Assets/Scripts/DayNightCycleManager.cs
+++ Prototypes/2D LWRP Prototype/Assets/Scripts/DayNightCycleManager.cs
@@ -10,7 +10,11 @@ public class DayNightCycleManager : MonoBehaviour
 	UnityEngine.Experimental.Rendering.LWRP.Light2D globalLight;
 	public Color[] colours;
 
-	
+	//public float transitionSpeed = 50f;
+
+	float lerpMax = 0.5f;
+		float lerpValue;
+	bool isChanging;
 
 	
 
@@ -19,13 +23,65 @@ public class DayNightCycleManager : MonoBehaviour
     {
 
 		globalLight = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
-
+		globalLight.color = colours[colourArrayIndex];
 		
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-		globalLight.color = colours[colourArrayIndex];
+
+		if (Input.GetKeyDown("space") && !isChanging)
+		{
+			isChanging = true;
+		}
+		if (isChanging)
+		{
+			ChangeColour();
+		}
+
+
+		//globalLight.color = Color.Lerp(colours[colourArrayIndex], colours[colourArrayIndex + 1], transitionSpeed * Time.deltaTime);
+		//colourArrayIndex++;
+
+		/*
+		for (int i = 0; i < colours.Length; i++)
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				globalLight.color = Color.Lerp(colours[i], colours[i + 1], transitionSpeed * Time.deltaTime);
+
+			}
+
+			
+		}
+		*/
+	    	
+		//globalLight.color = colours[colourArrayIndex];
 	}
+
+	
+	
+	void ChangeColour()
+	{
+		Debug.Log("Changing");
+		//globalLight.color = Color.Lerp(colours[colourArrayIndex], colours[colourArrayIndex + 1], transitionSpeed * Time.deltaTime);
+
+		if (lerpValue >= 1f)
+		{
+			colourArrayIndex++;
+			globalLight.color = colours[colourArrayIndex % colours.Length];
+			isChanging = false;
+			lerpValue = 0f;
+		}
+		else
+		{
+			lerpValue += lerpMax * Time.deltaTime;
+			int firstColor = colourArrayIndex % colours.Length;
+			int secondColor = (colourArrayIndex + 1) % colours.Length;
+			globalLight.color = Color.Lerp(colours[firstColor], colours[secondColor], lerpValue);
+		}
+
+	}
+	
 }
