48f8d44a25d4babe416183fab257598bae8e5699
Jamie <jmb1g17@soton.ac.uk>
2019-12-05 12:22:14 +0000

    Colour Array
    A slider can cycle between the colours in a defined array.

--- Prototypes/2D LWRP Prototype/Assets/Scripts/DayNightCycleManager.cs
+++ Prototypes/2D LWRP Prototype/Assets/Scripts/DayNightCycleManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class DayNightCycleManager : MonoBehaviour
 {
+	[Range(0, 9)]
+	public int colourArrayIndex;
+
 	UnityEngine.Experimental.Rendering.LWRP.Light2D globalLight;
+	public Color[] colours;
+
+	
+
+	
 
     // Start is called before the first frame update
     void Start()
     {
+
 		globalLight = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
 
-		globalLight.color = Color.blue;  
+		
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		globalLight.color = colours[colourArrayIndex];
+	}
 }
