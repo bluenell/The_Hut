04edac5c53fe799a6fdd1c5980da0101fb5534c4
Jamie <jmb1g17@soton.ac.uk>
2019-12-05 11:12:31 +0000

    Lighting prototpe
    Accessing the global light in script and changing the colour

---
+++ Prototypes/2D LWRP Prototype/Assets/Scripts/DayNightCycleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycleManager : MonoBehaviour
+{
+	UnityEngine.Experimental.Rendering.LWRP.Light2D globalLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		globalLight = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
+
+		globalLight.color = Color.blue;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
